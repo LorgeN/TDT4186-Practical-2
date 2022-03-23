e7fb5a082d0269bc43d4e269f707d944dbddb4cb
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-03-23 22:10:09 +0100

    feat: add ipv6 test from gh gist (wrong formatting)

--- src/mtwww.c
+++ src/mtwww.c
@@ -16,17 +16,20 @@
 #define DELIMITER " "
 #define FILE_NOT_FOUND_DEFAULT "<p>File not found</p>\n"
 
-typedef struct server_t {
+typedef struct server_t
+{
     int sock;
     struct sockaddr_in addr;
 } server_t;
 
-typedef struct conn_t {
-    struct sockaddr_in remote_addr;  // todo: remove this later
+typedef struct conn_t
+{
+    struct sockaddr_in remote_addr; // todo: remove this later
     struct sockaddr_in local_addr;
 } conn_t;
 
-struct mtwww_options_t {
+struct mtwww_options_t
+{
     char *path;
     int port;
     int worker_threads;
@@ -37,30 +40,37 @@ struct mtwww_options_t {
 struct mtwww_options_t opt;
 volatile sig_atomic_t active;
 
-void __shutdown_sig_handler(int sig) {
+void __shutdown_sig_handler(int sig)
+{
     active = 0;
 }
 
-int __file_isdir(const char *path) {
+int __file_isdir(const char *path)
+{
     struct stat st;
-    if (stat(path, &st) != 0) {
-        return 0;  // Not a directory
+    if (stat(path, &st) != 0)
+    {
+        return 0; // Not a directory
     }
 
     return S_ISDIR(st.st_mode);
 }
 
-int __file_isreg(const char *path) {
+int __file_isreg(const char *path)
+{
     struct stat st;
-    if (stat(path, &st) != 0) {
-        return 0;  // Not a directory
+    if (stat(path, &st) != 0)
+    {
+        return 0; // Not a directory
     }
 
     return S_ISREG(st.st_mode);
 }
 
-int __read_options(struct mtwww_options_t *opt, int argc, char **argv) {
-    if (argc != 5) {
+int __read_options(struct mtwww_options_t *opt, int argc, char **argv)
+{
+    if (argc != 5)
+    {
         printf("Invalid number of arguments %d, expected 4\n", argc - 1);
         printf("Usage: ./mtwwwd <www-path> <port> <worker_threads> <buffer_slots>\n");
         return 1;
@@ -71,25 +81,29 @@ int __read_options(struct mtwww_options_t *opt, int argc, char **argv) {
 
     path = argv[1];
     // Check if the path exists, and is a directory
-    if (!__file_isdir(path)) {
+    if (!__file_isdir(path))
+    {
         printf("%s does not exist/is not a directory\n", path);
         return 1;
     }
 
     tmp = argv[2];
-    if (!(port = atoi(tmp))) {
+    if (!(port = atoi(tmp)))
+    {
         printf("Invalid port number %s\n", tmp);
         return 1;
     }
 
     tmp = argv[3];
-    if (!(workers = atoi(tmp))) {
+    if (!(workers = atoi(tmp)))
+    {
         printf("Invalid worker thread count %s\n", tmp);
         return 1;
     }
 
     tmp = argv[4];
-    if (!(buffer_slots = atoi(tmp))) {
+    if (!(buffer_slots = atoi(tmp)))
+    {
         printf("Invalid buffer slot count %s\n", tmp);
         return 1;
     }
@@ -102,7 +116,8 @@ int __read_options(struct mtwww_options_t *opt, int argc, char **argv) {
     return 0;
 }
 
-int __read_and_send_file(char *filename, char *cwd, int client_socket) {
+int __read_and_send_file(char *filename, char *cwd, int client_socket)
+{
     FILE *fptr;
 
     char abs_path[512] = {0};
@@ -110,15 +125,18 @@ int __read_and_send_file(char *filename, char *cwd, int client_socket) {
     strncat(abs_path, filename, 512 - strlen(abs_path));
 
     // Handle case where file doesn't exist
-    if (!__file_isreg(abs_path)) {
+    if (!__file_isreg(abs_path))
+    {
         // Allow user to specify a 404 page
         memset(abs_path, 0, 512);
 
         strcpy(abs_path, cwd);
         strncat(abs_path, "/404.html", 512 - strlen(abs_path));
 
-        if (!__file_isreg(abs_path)) {
-            if (send(client_socket, FILE_NOT_FOUND_DEFAULT, strlen(FILE_NOT_FOUND_DEFAULT), 0) < 0) {
+        if (!__file_isreg(abs_path))
+        {
+            if (send(client_socket, FILE_NOT_FOUND_DEFAULT, strlen(FILE_NOT_FOUND_DEFAULT), 0) < 0)
+            {
                 return 1;
             }
 
@@ -126,7 +144,8 @@ int __read_and_send_file(char *filename, char *cwd, int client_socket) {
         }
     }
 
-    if ((fptr = fopen(abs_path, "rb")) == NULL) {
+    if ((fptr = fopen(abs_path, "rb")) == NULL)
+    {
         return 1;
     }
 
@@ -138,8 +157,10 @@ int __read_and_send_file(char *filename, char *cwd, int client_socket) {
     printf("%lu", size);
 
     char buffer[1000] = {0};
-    while (fgets(buffer, sizeof(buffer), fptr) != NULL) {
-        if (send(client_socket, buffer, strlen(buffer), 0) < 0) {
+    while (fgets(buffer, sizeof(buffer), fptr) != NULL)
+    {
+        if (send(client_socket, buffer, strlen(buffer), 0) < 0)
+        {
             return 1;
         }
     }
@@ -147,7 +168,8 @@ int __read_and_send_file(char *filename, char *cwd, int client_socket) {
     return 0;
 }
 
-void handle_connection(int file_descriptor) {
+void handle_connection(int file_descriptor)
+{
     printf("FD %d\n", file_descriptor);
 
     int socket_desc, client_size;
@@ -156,7 +178,8 @@ void handle_connection(int file_descriptor) {
     char client_message[2000] = {0};
 
     // Receive client's message:
-    if (recv(file_descriptor, client_message, sizeof(client_message), 0) < 0) {
+    if (recv(file_descriptor, client_message, sizeof(client_message), 0) < 0)
+    {
         printf("Couldn't receive request\n");
         return;
     }
@@ -168,8 +191,10 @@ void handle_connection(int file_descriptor) {
     char *token = strtok(client_message, DELIMITER);
 
     int tokenPosition = 0;
-    while (token != NULL) {
-        if (tokenPosition < 4) {
+    while (token != NULL)
+    {
+        if (tokenPosition < 4)
+        {
             // Avoid overflowing if the request is massive
             strncpy(request[tokenPosition], token, 256);
         }
@@ -184,9 +209,11 @@ void handle_connection(int file_descriptor) {
     close(file_descriptor);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // __read_options returns 1 if it fails, 0 otherwise
-    if (__read_options(&opt, argc, argv)) {
+    if (__read_options(&opt, argc, argv))
+    {
         return EXIT_FAILURE;
     }
 
@@ -208,11 +235,13 @@ int main(int argc, char **argv) {
 
     int client_sock, client_size;
     struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in6 addr6;
 
     // Create socket
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (socket_desc < 0) {
+    if (socket_desc < 0)
+    {
         printf("Error while creating socket\n");
         return -1;
     }
@@ -224,8 +253,13 @@ int main(int argc, char **argv) {
     server_addr.sin_port = htons(opt.port);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    addr6.sin6_family = AF_INET6;
+    addr6.sin6_port = htons(opt.port);
+    addr6.sin6_addr.s6_addr = in6addr_any;
+
     // Bind to the set port and IP:
-    if (bind(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         printf("Couldn't bind to the port\n");
         return -1;
     }
@@ -233,7 +267,8 @@ int main(int argc, char **argv) {
     printf("Done with binding\n");
 
     // Listen for clients:
-    if (listen(socket_desc, 1) < 0) {
+    if (listen(socket_desc, 1) < 0)
+    {
         printf("Error while listening\n");
         return -1;
     }
@@ -249,10 +284,12 @@ int main(int argc, char **argv) {
     pollfds[0].events = POLLIN | POLLPRI;
 
     active = 1;
-    while (active) {
+    while (active)
+    {
         // Accept an incoming connection
         client_sock = accept(socket_desc, (struct sockaddr *)&client_addr, &client_size);
-        if (client_sock < 0) {
+        if (client_sock < 0)
+        {
             continue;
         }
 
